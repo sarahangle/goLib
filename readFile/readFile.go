8f64f80daa3ade188f9dcef2668f3b3e0ecb46e6
sarahangle <SarahAngleEngineer@gmail.com>
2024-12-18 14:40:50 -0800

    ReadLines updated

--- readFile/readFile.go
+++ readFile/readFile.go
@@ -1,10 +1,23 @@
 package readFile
 
 import(
-	"fmt"
+	"os"
+	"bufio"
 )
 
-func ReadLines() {
-	fmt.Println("Hello from github goLib module, readFile!")
-	fmt.Println("Now I'm updated again!")
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func ReadLines(path string) ([]string, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
 }
